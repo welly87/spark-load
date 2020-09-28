79953c1e1e43deef3b3fd7ca250b8cbc64a85d9f
Welly Tambunan <if05041@gmail.com>
2020-09-28 14:26:47 +0700

    Update run-spark.sh

--- run-spark.sh
+++ run-spark.sh
@@ -1,1 +1,3 @@
 spark-shell --packages org.apache.kudu:kudu-spark2_2.11:1.12.0  --master local[*] --conf spark.driver.bindAddress=127.0.0.1 --conf spark.driver.host=127.0.0.1 --conf spark.driver.port=7777
+
+>load: load_data.scala
